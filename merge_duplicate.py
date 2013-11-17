a214697fa4adafce6cf25513e4c4da89af7fb3e2
sreenivas <sreenivas9alapati@gmail.com>
2013-11-17 10:58:43 +0530

    Create merge_duplicate.py

---
+++ merge_duplicate.py
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+# Merge Duplicate
+# Merge a copy of all selected layers into a new layer	
+# Like Command+Shift+E / Ctrl+Shift+E in photoshop
+# 
+# Written by sreenivas alapati
+# ------------------------------------------------------------------------------
+
+
