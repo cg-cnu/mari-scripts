5133ec8c88d1e2aa08c0a866734d142b2417516b
sreenivas <sreenivas9alapati@gmail.com>
2013-11-18 12:39:10 +0530

    Update merge_duplicate.py

--- merge_duplicate.py
+++ merge_duplicate.py
@@ -1,9 +1,33 @@
 # ------------------------------------------------------------------------------
 # Merge Duplicate
+# ------------------------------------------------------------------------------
+# 
 # Merge a copy of all selected layers into a new layer	
-# Like Command+Shift+E / Ctrl+Shift+E in photoshop
+# Command+Shift+E / Ctrl+Shift+E in photoshop
 # 
 # Written by sreenivas alapati
 # ------------------------------------------------------------------------------
 
+import mari
 
+def mergeDuplicateLayers():
+	curGeo = mari.geo.current()
+	curChan = curGeo.currentChannel()
+	curLayerName = str (curChan.currentLayer().name())
+	
+	mari.history.startMacro('Merge Duplicate')
+	
+	copyAction = mari.actions.find('/Mari/Layers/Copy')
+	copyAction.trigger()
+	
+	curChan.mergeLayers()
+	
+	if str (mari.app.version().string()) = '2.5v1':
+		curLayer = curChan.currentLayer()
+		curLayer.setName(curLayerName + '_mrgDup')
+	
+	mari.history.stopMacro()
+	
+mergeDuplicateAction = mari.actions.create ('Merge Duplicate'. 'mergeDuplicateLayers()')
+mari.menus.addAction(mergeDuplicateAction, 'MainWindow/Layers')
+mergeDuplicateAction.setShortcut('Ctrl+Shift+E')
