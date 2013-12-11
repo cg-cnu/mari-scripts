f361f6249eee73663722521f6b712ece01127b69
sreenivas alapati <sreenivas9alapati@gmail.com>
2013-12-11 12:36:15 +0530

    implemented the selected patches functionality and naming for 2.5

--- merge_duplicate.py
+++ merge_duplicate.py
@@ -3,6 +3,9 @@
 # ------------------------------------------------------------------------------
 # creates a merge duplicate of selected layers.	
 # Same as Command+Shift+E / Ctrl+Shift+E in photoshop
+# Will merge for selected patches other wise will merge all.
+# Will rename the merged layer to the current active layer name + _mrgDup
+# ** rename will work only for mari 2.5 **
 #
 # copy the script to the same location as your log folder in 
 # windows: C:\Users\[user_name]\Documents\Mari\Scripts
@@ -19,12 +22,23 @@
 import mari
 
 def mergeDuplicateLayers():
+	''' Creates a merge duplicate of the current selected layers '''
+	
+	if not mari.projects.current():
+		mari.utils.message('No project currently open')
+		return
+	
 	curGeo = mari.geo.current()
 	curChan = curGeo.currentChannel()
-
-	mari.history.startMacro('Merge Duplicate')
-	mari.app.setWaitCursor()
+	curLayer = curChan.currentLayer()
+	curActiveLayerName = str(curLayer.name())
 	
+	patches = list(curGeo.patchList())
+	unSelPatches = [ patch for patch in patches if not patch.isSelected() ]
+	
+	mari.app.setWaitCursor()
+	mari.history.startMacro('Merge Duplicate')
+
 	copyAction = mari.actions.find('/Mari/Layers/Copy')
 	copyAction.trigger()
 	
@@ -32,10 +46,27 @@ def mergeDuplicateLayers():
 	pasteAction.trigger()
 	
 	curChan.mergeLayers()
-		
-	mari.history.stopMacro()
-	mari.app.restoreCursor()	
 	
+	curLayer = curChan.currentLayer()
+
+	if len(patches) != len(unSelPatches):
+		
+		imgSet = curLayer.imageSet()
+		
+		for patch in unSelPatches:
+			uv = patch.uvIndex()
+			patchImg = imgSet.image(uv, -1)
+			patchImg.fill(mari.color(1,0,0,0))
+			
+	if mari.app.version().number() >= 20501300:
+		
+		curLayer.setName(curActiveLayerName + '_mrgDup')
+
+	mari.history.stopMacro()
+	mari.app.restoreCursor()
+
+	return
+
 mergeDuplicateAction = mari.actions.create ('Merge Duplicate', 'mergeDuplicateLayers()')
 mari.menus.addAction(mergeDuplicateAction, 'MainWindow/Layers')
 mergeDuplicateAction.setShortcut('Ctrl+Shift+E')
