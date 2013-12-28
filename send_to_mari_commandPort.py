3646f6f53d1c219cfe3c305cc7a786a7788e7bdd
sreenivas alapati <sreenivas9alapati@gmail.com>
2013-12-28 11:22:14 +0530

    Update send_to_mari_commandPort.py

--- send_to_mari_commandPort.py
+++ send_to_mari_commandPort.py
@@ -5,7 +5,7 @@ def toggleCommandPort():
 	mari.app.enableCommandPort( not mari.app.commandPortEnabled() )
 
 	if mari.app.commandPortEnabled():
-		print "command port enabled with the number", mari.app.commanPortNumber()
+		print "command port enabled with the number", mari.app.commandPortNumber()
 	else:
 		print "command port disabled"
 
