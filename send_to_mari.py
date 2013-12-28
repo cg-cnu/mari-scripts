916f0e12fbc5c94df06f15a94b83585ff164d3b8
sreenivas alapati <sreenivas9alapati@gmail.com>
2013-12-28 11:16:24 +0530

    Update send_to_mari.py

--- send_to_mari.py
+++ send_to_mari.py
@@ -1,14 +1,28 @@
-import os
-import sys
+#!/usr/bin/python
 from telnetlib import Telnet
+import sys
 
-connection = Telnet('localhost', 6100)
+try:
+	connection = Telnet('localhost', 6100)
+	#message = "mari.projects.current()\x04"
+	#connection.write(message)
+	#reply = connection.read()
+	#if reply:
+	images = sys.argv[1:]
 
-path = str(os.getcwd() + '/')
-images = sys.argv[1:]
+	for image in images:
 
-for image in images:
-	message = "mari.images.load('" + path + image + "')"
-	connection.write(message)
-	connection.write("\x04")
-tn.close()
+		message = "mari.images.load('" + image + "')"
+		connection.write(message)
+		connection.write("\x04")
+
+	connection.close()
+
+except:
+	import Tkinter
+	import tkMessageBox
+	window = Tkinter.Tk()
+	window.wm_withdraw()
+	tkMessageBox.showerror(title = 'Mari Error',
+							message = 'Enable command port in mari')
+							
