103318fd0f810981df2b2e58df97111ac963b66d
sreenivas alapati <sreenivas9alapati@gmail.com>
2013-12-28 21:57:57 +0530

    Update screenshot_all_channels.py

--- screenshot_all_channels.py
+++ screenshot_all_channels.py
@@ -1,27 +1,27 @@
 # ------------------------------------------------------------------------------
 # Screenshot all channels
 # ------------------------------------------------------------------------------
-# Takes screenshot of all the channels for the current object
-# Specify the path and setting in View > Screenshot Settings
-# *IMP*: Keep incremental -> Enabled 
+# Takes screenshot of all the channels for the current view.
+# Specify the path and setting in View > Screenshot Settings.
+# *IMP*: Keep incremental -> Enabled
 #
 # copy the script to the same location as your log folder in 
 # windows: C:\Users\[user_name]\Documents\Mari\Scripts
 # linux: /home/[user_name]/Mari/Scripts
 # Mac: /home/[Username]/Mari/Scripts
 #
-# Will create a menue item in view > Screenshot All Channels
+# a menue item in view > Screenshot All Channels will be created
 #
-# @uthor by sreenivas alapati (cg-cnu)
+# @uthor sreenivas alapati (cg-cnu)
 # ------------------------------------------------------------------------------
 
 import mari
 
-def screenshotAllChans():
-    '''Creates snapshot of all the channels on the current object '''
+def screenshotAllChannels():
+    '''Take screenshot of all the channels for the current view '''
 
     if mari.projects.current() == None:
-        mari.utils.message("No project currently open")
+        mari.utils.message("No project currently open", title = "Error")
         return
 
     curGeo = mari.geo.current()
@@ -32,18 +32,19 @@ def screenshotAllChans():
     mari.app.setWaitCursor()
 
     for chan in chanList:
-        chanName = str (chan.name())
+        
         curGeo.setCurrentChannel(chan)
         curCanvas.repaint()
-
+        
         snapAction = mari.actions.find ('/Mari/Canvas/Take Screenshot')
         snapAction.trigger()
 
     curGeo.setCurrentChannel(curChannel)
     curCanvas.repaint()
-
+    
     mari.app.restoreCursor()
 
     return
 
-mari.menus.addAction(mari.actions.create('Screenshot All Channels', 'screenshotAllChans()'), "MainWindow/View")
+mari.menus.addAction(mari.actions.create('Screenshot All Channels', 
+						'screenshotAllChannels()'), "MainWindow/View")
